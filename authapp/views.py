1c2002855799ddea4cb5c7b5428c9fc3a435872f
Egor_Ostroumov <araimo@yandex.ru>
2021-12-05 22:01:13 +0300

    Базовая админка

--- authapp/views.py
+++ authapp/views.py
@@ -2,7 +2,7 @@ from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from authapp.forms import UserLoginForm, UserRegisterForm, UserProfileForm
+from authapp.forms import UserLoginForm, UserRegisterForm, UserProfilerForm
 from django.urls import reverse
 # Create your views here.
 from baskets.models import Basket
@@ -51,7 +51,7 @@ def register(request):
 @login_required
 def profile(request):
     if request.method == 'POST':
-        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        form = UserProfilerForm(instance=request.user, data=request.POST, files=request.FILES)
         if form.is_valid():
             messages.success(request, 'Изменения сохранены')
             form.save()
@@ -60,7 +60,7 @@ def profile(request):
 
     context = {
         'title': 'Geekshop | profile',
-        'form': UserProfileForm(instance=request.user),
+        'form': UserProfilerForm(instance=request.user),
         'baskets': Basket.objects.filter(user=request.user)
     }
     return render(request, 'authapp/profile.html',context)
